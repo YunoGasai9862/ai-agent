89494fbc878135386f464b128df4a1a5ca32c7a9
Muhammad Bilal <74299910+YunoGasasi9862@users.noreply.github.com>
2025-07-28 12:45:32 +0300

    WIP

--- src/agent/main.py
+++ src/agent/main.py
@@ -1,7 +1,9 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from models.response import Response
+from src.models.response import Response
+from src.parser.output_parser import (OutputParser)
 import os
+from langchain.agents import create_tool_calling_agent, AgentExecutor
 
 load_dotenv()
 
@@ -13,3 +15,16 @@ llm = ChatOpenAI(model_name = os.environ['MODEL_NAME'],
                     max_retries = os.environ['MAX_RETRIES'],
                     api_key = os.environ["OPEN_AI_API_KEY"])
 
+output_parser = OutputParser()
+prompt = output_parser.get_default_chat_prompt_template(response_model=Response)
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[]
+)
+
+agent_executor = AgentExecutor(agent=agent, tools = [], verbose=True)
+
+raw_response = agent_executor.invoke({"query": "Are blackholes a portal to different universes"})
+print(raw_response)
--- src/parser/output_parser.py
+++ src/parser/output_parser.py
@@ -1,9 +1,29 @@
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain_core.prompts import ChatPromptTemplate
-from models.response import Response
+from typing import Any
+from abc import ABC
 
-parser = PydanticOutputParser(pydantic_object = Response)
-
-prompt = ChatPromptTemplate.from_template(
+class OutputParser(ABC) :
     
-)
+    def __init__(self):
+        super().__init__()
+        
+    def get_parser(self, response_model: Any) -> PydanticOutputParser:
+        return PydanticOutputParser(pydantic_object = response_model)
+    
+    def get_default_chat_prompt_template(self, response_model: Any) -> ChatPromptTemplate:
+        return ChatPromptTemplate.from_messages(
+                [("system",
+                    "Yuno's personal research assistance! Wrap the output in this format and provide no other text\n{format_instructions}" 
+                ),
+                (
+                    "placeholder", "{chat_history}"
+                ),
+                (
+                    "human", "{query}"
+                ),
+                (
+                    "placeholder", "{agent_scratchpad}"
+                )]
+            ).partial(format_instructions=self.get_parser(response_model).get_format_instructions())
+        
